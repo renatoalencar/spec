1ea34c4aecdaa787717b5d5460608b28f76ca13b
Renato Alencar <renatoalencar.73@gmail.com>
2022-07-14 15:28:13 -0300

    Add valid module

---
+++ lib/bs/valid/valid.d
@@ -0,0 +1,2 @@
+valid/valid.cmj : syntax/ast.cmj syntax/ast.cmi util/error.cmj util/error.cmi syntax/free.cmj syntax/free.cmi exec/i32.cmj exec/i32.cmi util/lib.cmj util/lib.cmi util/source.cmj util/source.cmi syntax/types.cmj syntax/types.cmi exec/v128.cmj exec/v128.cmi syntax/values.cmj syntax/values.cmi
+valid/valid.cmi :  syntax/ast.cmi  util/source.cmi
